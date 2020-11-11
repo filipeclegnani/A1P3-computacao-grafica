6112680f2b6672e5d4358220f374a903093fb961
Filipe Casaletti Legnani <filipeclegnani@gmail.com>
2020-11-11 14:36:28 -0300

    Começando a criação de formas e fazendo alguns testes legais

--- main.cpp
+++ main.cpp
@@ -3,9 +3,18 @@
 #include <windows.h>
 #include <gl/gl.h>
 #include <gl/glu.h>
+#include <math.h>
+
+#include "parametros.h"
 
 
 LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+
+void desenhaCirculo(PONTO centro, float raio, COR c);
+void desenhaLinha(PONTO a, PONTO b);
+void desenhaMundo();
+
+float rai = 0;
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
 
@@ -21,7 +30,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
 	                wc.lpszClassName,
 	                "My OpenGL App",
 	                WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-	                0, 0, 800, 600,
+	                0, 0, 600, 600,
 	                NULL, NULL, NULL, NULL
 	            );
 	HDC dc;
@@ -29,7 +38,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
 	PIXELFORMATDESCRIPTOR pfd;
 	pfd.nSize = sizeof(pfd);
 	pfd.nVersion = 1;
-	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
 	pfd.iPixelType = PFD_TYPE_RGBA;
 	pfd.cColorBits = 32;
 
@@ -50,6 +59,8 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
 
 	ShowWindow(hwnd, ncmdshow);
 
+	SetTimer(hwnd, SET_TIMER_60, 1000/60, NULL);
+	
 	MSG msg = {};
 
 	while(GetMessage(&msg, NULL, 0, 0)) {
@@ -61,7 +72,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
 }
 
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
-
+	
 	switch (msg) {
 
 		case WM_CREATE:
@@ -70,13 +81,19 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
 
 
 		case WM_PAINT:
-			
-			
 			//redesenhatodas();
 			break;
 
 		case WM_MOUSEMOVE:
 			SetCursor(LoadCursor(NULL, IDC_ARROW));
+			break;
+		case WM_TIMER:
+			switch(wp){
+				case SET_TIMER_60:
+					desenhaMundo();
+					SwapBuffers(GetDC(hwnd));
+					break;
+			}
 			break;
 		case WM_SIZE:
 			PostMessage(hwnd, WM_PAINT, 0, 0);
@@ -90,3 +107,65 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
 
 	return DefWindowProc(hwnd, msg, wp, lp);
 }
+
+void desenhaCirculo(PONTO centro, float raio, COR c){
+
+	GLfloat vertices = 100;
+	GLfloat angulo;
+	
+	glBegin(GL_POLYGON);
+	glColor3f(c.R, c.G, c.B);
+	for(int i=0; i < vertices; i++) {
+		angulo = 2 * PI * i / vertices;
+		glVertex2f(
+		    (cos(angulo) * (raio)) + centro.x,
+		    (sin(angulo) * (raio)) + centro.y);
+	}
+	glEnd();
+	glFlush();
+}
+
+void desenhaLinha(PONTO a, PONTO b){
+	glBegin(GL_LINES);
+	glColor3f (1, 1, 1);
+	glVertex2f(a.x, a.y);
+	glVertex2f(b.x, b.y);
+	glEnd();
+	glFlush();
+}
+
+void desenhaMundo(){
+	PONTO p, a, b;
+	COR c;
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	p.x = 0.5f;
+	p.y = 0.3f;
+	
+	a.x = 0.5f;
+	a.y = 0.3f;
+	
+	b.x = cos(2 * PI * rai / 100) * (0.5f);
+	b.y = sin(2 * PI * rai / 100) * (0.5f);
+	
+	if(rai < 100){
+		rai++;
+	}else{
+		rai = 0;
+	}
+	
+	c.R = 1;
+	c.G = 0;
+	c.B = 0;
+	
+	desenhaCirculo(p, 0.5f, c);
+	desenhaLinha(a, b);
+	
+	c.R = 0.4f;
+	c.G = 0.3f;
+	c.B = 1;
+	desenhaCirculo(b, 0.1f, c);
+}
+
+
+
---
+++ parametros.h
@@ -0,0 +1,38 @@
+#ifndef PARAMETROS_H
+#define PARAMETROS_H
+
+#define PI 3.1415
+
+#define SET_TIMER_60 60
+
+typedef struct {
+	float R;
+	float G;
+	float B;
+} COR;
+
+typedef struct {
+	float x;
+	float y;
+}PONTO;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // deve ser a ultima linha do arquivo
