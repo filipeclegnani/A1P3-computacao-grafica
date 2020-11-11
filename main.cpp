9199e4d1a1437954f1c8b95368f01931e8314980
Filipe Casaletti Legnani <filipeclegnani@gmail.com>
2020-11-11 16:16:51 -0300

    Desenhando o Stickman

--- main.cpp
+++ main.cpp
@@ -13,8 +13,9 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
 void desenhaCirculo(PONTO centro, float raio, COR c);
 void desenhaLinha(PONTO a, PONTO b);
 void desenhaMundo();
+void startaStickman();
 
-float rai = 0;
+STICKMAN henry = {0};
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
 
@@ -76,7 +77,7 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
 	switch (msg) {
 
 		case WM_CREATE:
-			
+			startaStickman();
 			break;
 
 
@@ -108,8 +109,58 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
 	return DefWindowProc(hwnd, msg, wp, lp);
 }
 
-void desenhaCirculo(PONTO centro, float raio, COR c){
+void startaStickman(){
+	// cabeça
+	henry.cabeca.x = 0.0f;
+	henry.cabeca.y = 0.5f;
+	
+	//ombro
+	henry.ombro.x = distanciax(H_PESCOCO, 270, henry.cabeca.x);
+	henry.ombro.y = distanciay(H_PESCOCO, 270, henry.cabeca.y);
+	
+	//cotovelos
+	henry.cotovelo1.x = distanciax(H_BRACOS, 280, henry.ombro.x);
+	henry.cotovelo1.y = distanciay(H_BRACOS, 280, henry.ombro.y);
+	
+	henry.cotovelo2.x = distanciax(H_BRACOS, 260, henry.ombro.x);
+	henry.cotovelo2.y = distanciay(H_BRACOS, 260, henry.ombro.y);
+	
+	//mãos
+	henry.mao1.x = distanciax(H_ANTEBRACO, 275, henry.cotovelo1.x);
+	henry.mao1.y = distanciay(H_ANTEBRACO, 275, henry.cotovelo1.y);
+	
+	henry.mao2.x = distanciax(H_ANTEBRACO, 265, henry.cotovelo2.x);
+	henry.mao2.y = distanciay(H_ANTEBRACO, 265, henry.cotovelo2.y);
+	
+	//quadril
+	henry.quadril.x = distanciax(H_TRONCO, 270, henry.ombro.x);
+	henry.quadril.y = distanciay(H_TRONCO, 270, henry.ombro.y);
+	
+	//joelho
+	henry.joelho1.x = distanciax(H_COXAS, 280, henry.quadril.x);
+	henry.joelho1.y = distanciay(H_COXAS, 280, henry.quadril.y);
+	
+	henry.joelho2.x = distanciax(H_COXAS, 260, henry.quadril.x);
+	henry.joelho2.y = distanciay(H_COXAS, 260, henry.quadril.y);
+	
+	//calcanhar
+	henry.calcanhar1.x = distanciax(H_PANTURRILHA, 270, henry.joelho1.x);
+	henry.calcanhar1.y = distanciay(H_PANTURRILHA, 270, henry.joelho1.y);
+	
+	henry.calcanhar2.x = distanciax(H_PANTURRILHA, 270, henry.joelho2.x);
+	henry.calcanhar2.y = distanciay(H_PANTURRILHA, 270, henry.joelho2.y);
+	
+	//pé
+	henry.pe1.x = distanciax(H_PES, 0, henry.calcanhar1.x);
+	henry.pe1.y = distanciay(H_PES, 0, henry.calcanhar1.y);
+	
+	henry.pe2.x = distanciax(H_PES, 0, henry.calcanhar2.x);
+	henry.pe2.y = distanciay(H_PES, 0, henry.calcanhar2.y);
+}
 
+void desenhaCirculo(PONTO centro, float raio, COR c){
+	// (cos(2 * PI * a / 360) * (r)) + px)
+	// (sin(2 * PI * a/ 360) * (r)) + py)
 	GLfloat vertices = 100;
 	GLfloat angulo;
 	
@@ -134,37 +185,52 @@ void desenhaLinha(PONTO a, PONTO b){
 	glFlush();
 }
 
+PONTO pto(float x, float y){
+	PONTO a;
+	a.x = (float)x;
+	a.y = (float)y;
+	return a;
+}
+
 void desenhaMundo(){
-	PONTO p, a, b;
-	COR c;
-	glClear(GL_COLOR_BUFFER_BIT);
+	// cabeça
+	desenhaCirculo(henry.cabeca, RAIO_CABECA, T_COLOR_WHITE);
 	
-	p.x = 0.5f;
-	p.y = 0.3f;
+	desenhaLinha(henry.cabeca, henry.ombro);
+	//ombro
+	desenhaCirculo(henry.ombro, RAIO_JUNTA, T_COLOR_RED);
 	
-	a.x = 0.5f;
-	a.y = 0.3f;
+	desenhaLinha(henry.ombro, henry.cotovelo1);
+	desenhaLinha(henry.ombro, henry.cotovelo2);
+	//cotovelos
+	desenhaCirculo(henry.cotovelo1, RAIO_JUNTA, T_COLOR_RED);
+	desenhaCirculo(henry.cotovelo2, RAIO_JUNTA, T_COLOR_RED);
 	
-	b.x = cos(2 * PI * rai / 100) * (0.5f);
-	b.y = sin(2 * PI * rai / 100) * (0.5f);
+	desenhaLinha(henry.cotovelo1, henry.mao1);
+	desenhaLinha(henry.cotovelo2, henry.mao2);
+	//mãos
 	
-	if(rai < 100){
-		rai++;
-	}else{
-		rai = 0;
-	}
+	//quadril
+	desenhaLinha(henry.ombro, henry.quadril);
 	
-	c.R = 1;
-	c.G = 0;
-	c.B = 0;
+	desenhaCirculo(henry.quadril, RAIO_JUNTA, T_COLOR_RED);
 	
-	desenhaCirculo(p, 0.5f, c);
-	desenhaLinha(a, b);
+	desenhaLinha(henry.quadril, henry.joelho1);
+	desenhaLinha(henry.quadril, henry.joelho2);
+	//joelho
+	desenhaCirculo(henry.joelho1, RAIO_JUNTA, T_COLOR_RED);
+	desenhaCirculo(henry.joelho2, RAIO_JUNTA, T_COLOR_RED);
 	
-	c.R = 0.4f;
-	c.G = 0.3f;
-	c.B = 1;
-	desenhaCirculo(b, 0.1f, c);
+	//calcanhar
+	desenhaLinha(henry.joelho1, henry.calcanhar1);
+	desenhaLinha(henry.joelho2, henry.calcanhar2);
+	
+	//pé
+	desenhaLinha(henry.calcanhar1, henry.pe1);
+	desenhaLinha(henry.calcanhar2, henry.pe2);
+	
+	
+	desenhaLinha(henry.quadril, pto(0.5f, 0.5f));
 }
 
 
--- parametros.h
+++ parametros.h
@@ -5,6 +5,22 @@
 
 #define SET_TIMER_60 60
 
+#define T_COLOR_RED (COR){1.0f, 0.0f, 0.0f}
+#define T_COLOR_WHITE (COR){1.0f, 1.0f, 1.0f}
+
+#define RAIO_CABECA 0.11f
+#define RAIO_JUNTA 0.013f
+#define H_PESCOCO		0.20f
+#define H_BRACOS		0.16f
+#define H_ANTEBRACO		0.16f
+#define H_TRONCO		0.30f
+#define H_COXAS			0.16f
+#define H_PANTURRILHA	0.16f
+#define H_PES			0.03f
+
+#define distanciax(r,a,p) ((cos(2*PI*a/360)*(r))+p)
+#define distanciay(r,a,p) ((sin(2*PI*a/360)*(r))+p)
+
 typedef struct {
 	float R;
 	float G;
@@ -16,6 +32,21 @@ typedef struct {
 	float y;
 }PONTO;
 
+typedef struct{
+	PONTO cabeca;
+	PONTO ombro;
+	PONTO quadril;
+	PONTO cotovelo1;
+	PONTO cotovelo2;
+	PONTO mao1;
+	PONTO mao2;
+	PONTO joelho1;
+	PONTO joelho2;
+	PONTO calcanhar1;
+	PONTO calcanhar2;
+	PONTO pe1;
+	PONTO pe2;
+}STICKMAN;
 
 
 
